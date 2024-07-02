c10f6489177ba1fe500047b76f0e610e54b78306
MaxFullstak <backmaxfront@gmail.com>
2024-07-02 16:52:25 +0300

    finish to code IPhone model section

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { yellowImg } from '../utils'
 import ModelView from './ModelView'
 
@@ -8,6 +8,7 @@ import { View } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import * as THREE from 'three'
 import { models, sizes } from '../constants'
+import { animateWithGsapTimeline } from '../utils/animations'
 
 const Model = () => {
 	const [size, setSize] = useState('small')
@@ -28,6 +29,24 @@ const Model = () => {
 	// rotation
 	const [smallRotation, setSmallRotation] = useState(0)
 	const [largeRotation, setLargeRotation] = useState(0)
+
+	const tl = gsap.timeline()
+
+	useEffect(() => {
+		if (size === 'large') {
+			animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+				transform: 'translateX(-100%)',
+				duration: 2,
+			})
+		}
+
+		if (size === 'small') {
+			animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+				transform: 'translateX(0)',
+				duration: 2,
+			})
+		}
+	}, [size])
 
 	useGSAP(() => {
 		gsap.to('#heading', { y: 0, opacity: 1 })
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,10 @@
-import * as THREE from 'three'
-
 import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei'
+
 import { Suspense } from 'react'
-import IPhoneModel from './IPhone'
+import * as THREE from 'three'
+import IPhone from './IPhone'
 import Lights from './Lights'
+import Loader from './Loader'
 
 const ModelView = ({
 	index,
@@ -11,16 +12,16 @@ const ModelView = ({
 	gsapType,
 	controlRef,
 	setRotationState,
-	item,
 	size,
+	item,
 }) => {
 	return (
 		<View
 			index={index}
 			id={gsapType}
-			className={`w-full h-full ${index === 2} ? 'right-[-10%]' : ''`}
+			className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
 		>
-			{/* Ambient light */}
+			{/* Ambient Light */}
 			<ambientLight intensity={0.3} />
 
 			<PerspectiveCamera makeDefault position={[0, 0, 4]} />
@@ -39,17 +40,15 @@ const ModelView = ({
 
 			<group
 				ref={groupRef}
-				name={`${index === 1} ? 'small' : 'large'`}
+				name={`${index === 1} ? 'small' : 'large`}
 				position={[0, 0, 0]}
 			>
-				<Suspense
-					fallback={
-						<Html>
-							<div>Loading</div>
-						</Html>
-					}
-				>
-					<IPhoneModel scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} />
+				<Suspense fallback={<Loader />}>
+					<IPhone
+						scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+						item={item}
+						size={size}
+					/>
 				</Suspense>
 			</group>
 		</View>
